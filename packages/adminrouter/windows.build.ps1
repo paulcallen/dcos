aa5a8004ddbc5bddb0b2be727f026312af82983b
Paul Allen <paulall@microsoft.com>
2018-08-10 14:42:57 -0700

    Update adminrouter for Windows
    * Move build to install apache web server and associated configuration
    * Add a Windows systemd service unit for running adminrouter on windows

--- packages/adminrouter/windows.build.ps1
+++ packages/adminrouter/windows.build.ps1
@@ -1,3 +1,10 @@
 $ErrorActionPreference = "stop"
+
+. c:\opt\mesosphere\environment.export.ps1
+
 New-Item -ItemType Directory "$env:PKG_PATH/bin/Apache24/conf"
-Copy-Item "pkg/extra/apache-windows/adminrouter.conf" "$env:PKG_PATH/bin/Apache24/conf"
+Copy-Item "/pkg/extra/apache-windows/adminrouter.conf" "$env:PKG_PATH/bin/Apache24/conf"
+
+# Copy service unit file
+new-item -ItemType Directory -Force "$env:PKG_PATH\dcos.target.wants"
+Copy-Item "c:\pkg\extra\systemd\dcos-adminrouter-agent.windows.service" "$env:PKG_PATH\dcos.target.wants\dcos-adminrouter-agent.service"
