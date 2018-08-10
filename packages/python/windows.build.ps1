cfa51956072bbea9b6ea8866dae95e79629300d3
Paul Allen <paulall@microsoft.com>
2018-08-10 15:19:18 -0700

    Python install for Windows, with associated pip and setuptools
    Python 3.6.5 has pip and setuptools included but on Windows we
    needed to install python to a temporary directory and copy to the
    final directory as part of buiding. This broke pip and setuptools
    and required the setups to be broken into two, one for core python,
    and the second to bootstrap the install of pip and setup.

--- packages/python/windows.build.ps1
+++ packages/python/windows.build.ps1
@@ -1,25 +1,44 @@
+
 $ErrorActionPreference = "stop"
+. c:\opt\mesosphere\environment.export.ps1
+$LIB_INSTALL_DIR="$env:PKG_PATH\lib\site-packages"  
+new-item -force -itemtype directory -path $LIB_INSTALL_DIR > $null
 
-Set-Location "c:/pkg/src"
-Write-Host "Installing python to directory ${env:PKG_PATH}"
+new-item -force -ItemType Directory c:\tmppip > $null
 
-$tmpdir = "c:\python363.tmp"
-
-$parameters = @{
-    'FilePath' = "c:/pkg/src/python/python-3.6.3.exe"
-
-    'ArgumentList' = @("/quiet", "/passive", "InstallAllUsers=1", "PrependPath=1", "Include_test=0", "Include_tcltk=0", "TargetDir=$tmpdir")
-    'Wait' = $true
-    'PassThru' = $true
-}
-$p = Start-Process @parameters
-if ($p.ExitCode -ne 0) {
-    Throw "Failed to install python-3.6.3"
+$params = @("-m", "ensurepip", "--root", "c:\tmppip")
+& "python.exe" $params
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "Failed to install pip and setuptools"
+    exit -1
 }
 
-& "$tmpdir\scripts\pip" install "--no-deps" "--install-option=`"--prefix=$tmpdir`"" "c:\pkg\src\cython\Cython-0.27.3"
-if ($LASTEXITCODE -ne 0) {
-    Throw "Failed to install Cython-0.27.3"
+#temporarily add pip modules to the path for cython install. 
+#next package will find it in the proper place.
+$env:PYTHONPATH+=";c:\tmppip\opt\mesosphere\bin\lib\site-packages"
+
+new-item -force -ItemType Directory c:\tmpPackage > $null
+Move-Item "c:\tmppip\opt\mesosphere\bin\scripts" "c:\tmppip\opt\mesosphere\bin\bin"
+copy-item "c:\tmppip\opt\mesosphere\bin\bin\pip3.exe" "c:\tmppip\opt\mesosphere\bin\bin\pip.exe"
+copy-item "c:\tmppip\opt\mesosphere\bin\bin\easy_install-3.6.exe" "c:\tmppip\opt\mesosphere\bin\bin\easy_install.exe"
+Copy-Item -force -Recurse "c:\tmppip\opt\mesosphere\bin\*" "$env:PKG_PATH\"
+
+$params = @("-m", "pip", "install", "--no-deps", "--install-option=`"--prefix=c:\tmpPackage`"", "--root=c:\", "c:\pkg\src\cython")
+& "python.exe" $params
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "Failed to install cython"
+    exit -1
 }
 
-copy-item -recurse -path "$tmpdir\*" -destination "$env:PKG_PATH\"
+$params = @("-m", "pip", "install", "--no-deps", "--install-option=`"--prefix=c:\tmpPackage`"", "--root=c:\", "c:\pkg\src\pywin32-ctypes")
+& "python.exe" $params
+if ($LASTEXITCODE -ne 0)
+{
+    Write-Error "Failed to install pywin32-ctypes"
+    exit -1
+}
+
+Move-Item "c:\tmpPackage\scripts" "c:\tmpPackage\bin"
+Copy-Item -force -Recurse "c:\tmpPackage\*" "$env:PKG_PATH\"
